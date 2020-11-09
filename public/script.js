b92777382df4a30af815aa5b33752aa261813aae
Ezaki Yuriko <e.guriko920@gmail.com>
2020-11-10 01:13:15 +0900

    add web app

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-const { connect } = require("http2")
-
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
@@ -8,15 +6,28 @@ const myPeer = new Peer(undefined, {
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
-
+const peers = {}
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true,
+    audio: false,
 }).then(stream => {
     addVideoStream(myVideo, stream)
+
+    myPeer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
     socket.on('user-connected', userId => {
         connectToNewUser(userId, stream)
     })
+})
+
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close()
 })
 
 myPeer.on('open', id => {
@@ -32,6 +43,8 @@ function connectToNewUser(userId, stream) {
     call.on('close', () => {
         video.remove()
     })
+
+    peers[userId] = call
 }
 
 function addVideoStream(video, stream) {
